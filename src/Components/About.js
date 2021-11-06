21cc2644ca4eb2f5b5047b8b0900a47b596a4c69
achraftarifa98 <achraf.tarifa@enis.tn>
2021-11-06 23:11:12 +0100

    wip
    Signed-off-by: achraftarifa98 <achraf.tarifa@enis.tn>

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,53 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="profile-pic"
+              src={profilepic}
+              alt="Achraf Tarifa Profile Pic"
+            />
+          </div>
+          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+            <div>
+              Hello there, happy to see you &#128512;
+              <br />
+              My name is achraf tarifa a third year computer science student and
+              project manager of the IEEE ENIS Student Branch. I am currently
+              looking for an internship for my graduation. As I think this is an
+              opportunity to improve myself so I am open to any offer that
+              challenges or requires me to work outside of my Knowledge set.{" "}
+              <br />
+              I’ve always been passionate about technology and as a future
+              software engineer, I believe that Technology has the ability to
+              impact lives at a level and scale that has never been realized in
+              the history of mankind so The idea that something I create can
+              impact someone across the world now, or in the future is what
+              drives my passion for Technology.
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+        <div className="row">
+          <div className="mx-auto download">
+            <p>
+              <a href={resumeDownload} className="button">
+                <i className="fa fa-download"></i>Download Resume
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     );
   }
 }
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,77 +1,168 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
 class Resume extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>
+              {education.description ?? (
+                <p>
+                  As a software engineering student at ENIS I was able to learn
+                  different technologies and gain new skills in various domains
+                  such as : <br />
+                  <b>Computer science:</b> What is programming? How do computers
+                  and the Internet work? What’s an algorithm, and how do you
+                  write one? What are data structures? Why are there so many?
+                  When do you use each one? Why is it important?
+                  <br />
+                  <b>UML:</b> specify, visualize, construct, and document the
+                  artifacts of software systems,as well as for business modeling
+                  and other non-software systems.
+                  <br />
+                  <b>Web development:</b> How does a web application work? What
+                  are the different web languages? HTML, CSS, Sass, JavaScript,
+                  SQL, databases, servers, APIs… how do they all fit together?
+                  <br />
+                  <b>Full-stack web development:</b>How to build an API and
+                  integrate it, Spring boot, Angular, mongodb
+                  <br />
+                  <b>Testing and deployment:</b> How to deploy an application
+                  for everyone to use? What are the different running
+                  environments (development, test, production)? How to properly
+                  test an application? how to package applications into
+                  containers with Docker and Kubernetes (Devops)
+                  <br />
+                  <b>Artificial intelligence:</b> learn machine learning
+                  algorithms and deep learning neural networks
+                </p>
+              )}
+            </p>
+          </div>
+        );
+      });
+      var work = this.props.data.work.map(function (work) {
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
+            <p className="info">
+              {work.title}
+              <span>&bull;</span> <em className="date">{work.years}</em>
+            </p>
             <p>{work.description}</p>
-        </div>
-      })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+            <p>
+              <b className="date bold">KEYWORDS: </b>
+              {work.keywords}
+            </p>
+          </div>
+        );
+      });
+      var projects = this.props.data.projects.map(function (projects) {
+        return (
+          <div key={projects.name}>
+            <h3>{projects.name}</h3>
+            <p className="info">
+              <em className="date">{projects.time}</em>
+            </p>
+            <p>{projects.description}</p>
+            <p>
+              <b className="date bold">Technologies: </b>
+              {projects.keywords}
+            </p>
+          </div>
+        );
+      });
+      var communitylife = this.props.data.communitylife.map(function (communitylife) {
+        return (
+          <div key={communitylife.company}>
+            <h3>{communitylife.company}</h3>
+            <p className="info">
+              {communitylife.title}
+              <span>&bull;</span>{" "}
+              <em className="date">{communitylife.years}</em>
+            </p>
+            <p>{communitylife.description}</p>
+          </div>
+        );
+      });
+      var skills = this.props.data.skills.map(function (skills) {
+        var className = "bar-expand " + skills.name.toLowerCase();
+        return (
+          <li key={skills.name}>
+            <span style={{ width: skills.level }} className={className}></span>
+            <em>{skills.name}</em>
+          </li>
+        );
+      });
     }
 
     return (
       <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">{education}</div>
             </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+          </div>
         </div>
-    </div>
 
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
 
+          <div className="nine columns main-col">{work}</div>
+        </div>
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>academic projects</span>
+            </h1>
+          </div>
 
-      <div className="row skill">
+          <div className="nine columns main-col">{projects}</div>
+        </div>
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Community Life</span>
+            </h1>
+          </div>
 
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
+          <div className="nine columns main-col">{communitylife}</div>
+        </div>
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
+            <p>{skillmessage}</p>
 
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+            <div className="bars">
+              <ul className="skills">{skills}</ul>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
